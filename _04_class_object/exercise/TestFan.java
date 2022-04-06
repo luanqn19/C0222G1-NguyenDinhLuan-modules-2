9c16f73aa471f9f097f03b5a864949edf6a7ae1c
luanqn19 <luanqn20@gmail.com>
2022-04-06 10:54:23 +0700

    Reupdate ex _04_class_object

--- _04_class_object/exercise/TestFan.java
+++ _04_class_object/exercise/TestFan.java
@@ -3,18 +3,18 @@ package _04_class_object.exercise;
 public class TestFan {
     public static void main(String[] args) {
         Fan fan1 = new Fan();
-        fan1.setSpeed(3);
+        fan1.setSpeed(Fan.HIGH);
         fan1.setRadius(10);
         fan1.setColor("yellow");
         fan1.setStatus(true);
-        System.out.println(fan1.toString());
+        System.out.println(fan1);
 
         System.out.println();
         Fan fan2 = new Fan();
-        fan2.setSpeed(2);
+        fan2.setSpeed(Fan.MEDIUM);
         fan2.setRadius(5);
         fan2.setColor("blue");
         fan2.setStatus(false);
-        System.out.println(fan2.toString());
+        System.out.println(fan2);
     }
 }
